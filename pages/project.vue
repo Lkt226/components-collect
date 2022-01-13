7e1e8ae708dd6fd13f86cb961afb4bdfd86c0f5f
lkt226 <victorhugods.pereira@gmail.com>
2022-01-12 23:25:51 -0300

    go to github project

--- pages/project.vue
+++ pages/project.vue
@@ -4,6 +4,7 @@
         <div>
             <component :is="id" class="grid-area"/>
         </div>
+        <button class="c-info" @click="goToCode(id)" :style="`--i: ${id.replace('c', '')};`">Ver o codigo</button>
     </div>
     <h1 v-else>loading ...</h1>
 </template>
@@ -20,6 +21,9 @@ export default defineComponent({
     methods:{
         goHome(){
             this.$router.push('/')
+        },
+        goToCode(id){
+            window.open(`https://github.com/Lkt226/components/blob/master/projects/${id}.vue`, '_blank')
         }
     }
 })
@@ -27,15 +31,26 @@ export default defineComponent({
 
 <style lang="scss" scoped>
     #body{
-        @apply flex gap-8 p-12 min-h-100vh
+        @apply flex flex-col gap-8 p-12 min-h-100vh
                 justify-center items-center;
 
         .grid-area{
             @apply bg-light-600 p-6 shadow-lg relative;
         }
-
         .go-back{
-            @apply fixed top-4 left-4 bg-gray-200
+            @apply fixed top-4 left-4;
+        }
+
+        .c-info{
+            &::before{
+                counter-reset: i var(--i);
+                content: 'id:c'counter(i);
+                @apply pr-4;
+            }
+        }
+
+        button{
+            @apply  bg-gray-200
                     p-3 mb-2 rounded-sm
                     font-bold font-mono text-center text-gray-500; 
 
