21f94832710982700a83859a953715f7ca6be119
lkt226 <victorhugods.pereira@gmail.com>
2022-01-18 11:24:25 -0300

    fonts

--- app.vue
+++ app.vue
@@ -4,7 +4,19 @@
 
 
 <style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@700&family=Sora&display=swap');
+
   html, body, #__nuxt{
       overflow: hidden;
+  }
+
+  *{
+    font-family: 'Sora', sans-serif;
+    font-weight: normal;
+  }
+
+  h1,h2,h3{
+    font-family: 'Kanit', sans-serif;
+    font-weight: bold;
   }
 </style>
